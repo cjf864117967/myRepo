46c0265987eeaba22f0c1648d8abbe70aeab8dc3
陈 <864117967@qq.com>
2019-07-13 09:52:48 +0800

    two

--- Day7.12LianXi/src/main/java/com/bw/exception/AsserUtil.java
+++ Day7.12LianXi/src/main/java/com/bw/exception/AsserUtil.java
@@ -1,11 +1,31 @@
 package com.bw.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class AsserUtil {
+	/**
+	 * 
+	 * @Title: Istrue 
+	 * @Description: TODO
+	 * @param exp
+	 * @param message
+	 * @return: void
+	 */
 	public static void Istrue(boolean exp,String message) {
 		if(!exp){
 			 throw new CRuntimeException(message);
 		}
 	}
+	/**
+	 * 
+	 * @Title: Isfalse 
+	 * @Description: TODO
+	 * @param exp
+	 * @param message
+	 * @return: void
+	 */
 	public static void Isfalse(boolean exp,String message) {
 		if(exp){
 			throw new CRuntimeException(message);
@@ -14,6 +34,57 @@ public class AsserUtil {
 	
 	public static void isNotNull(Object exp,String message){
 		if(exp==null){
+			throw new CRuntimeException(message);
+		}
+	}
+	/**
+	 * 
+	 * @Title: 断言List,Set集合不为空 
+	 * @Description: TODO
+	 * @return: void
+	 */
+	public static void collectionNotNull(ArrayList list,String message) {
+		if(list.size()==0){
+			
+			throw new CRuntimeException(message);
+		}
+	}
+	/**
+	 * 
+	 * @Title: MapNotNull 
+	 * @Description: 断言 Map 集合不为空
+	 * @param map
+	 * @param message
+	 * @return: void
+	 */
+	public static void MapNotNull(Map map,String message) {
+		if(map.size()==0){
+			throw new CRuntimeException(message);
+		}
+	}
+	/**
+	 * 
+	 * @Title: isLength 
+	 * @Description: 断言字符串必须有值，去掉空格后，长度必须大于 0
+	 * @param exp
+	 * @param message
+	 * @return: void
+	 */
+	public static void isLength(String exp,String message) {
+		if(!(exp.trim().length()>0)){
+			throw new CRuntimeException(message);
+		}
+	}
+	/**
+	 * 
+	 * @Title: isZheng 
+	 * @Description: 断言值必须是正数(值大于 0)
+	 * @param exp
+	 * @param message
+	 * @return: void
+	 */
+	public static void isZheng(Integer exp,String message) {
+		if(!(exp>0)){
 			throw new CRuntimeException(message);
 		}
 	}
--- Day7.12LianXi/src/test/java/com/bw/test/TestAsserUtil.java
+++ Day7.12LianXi/src/test/java/com/bw/test/TestAsserUtil.java
@@ -1,5 +1,10 @@
 package com.bw.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 import com.bw.exception.AsserUtil;
@@ -32,6 +37,51 @@ public class TestAsserUtil {
 		try {
 			AsserUtil.isNotNull("", "为空");
 		} catch (CRuntimeException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	@Test
+	public void collectionNotNull(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("1");
+		try {
+			AsserUtil.collectionNotNull(list, "为空");
+		} catch (CRuntimeException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void MapNotNull(){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("123", 1);
+		try {
+			AsserUtil.MapNotNull(map, "为空");
+		} catch (CRuntimeException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void isLength(){
+		try {
+			AsserUtil.isLength(" 1", "字符串没有值");
+		} catch (CRuntimeException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void isZheng(){
+		try {
+			AsserUtil.isZheng(1, "不是正数");
+		} catch (CRuntimeException e) {
+			
 			// TODO: handle exception
 			e.printStackTrace();
 		}
